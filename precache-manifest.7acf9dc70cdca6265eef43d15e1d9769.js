354357d5330fb69cd5731f1f9c71f6427faeee4e
Jainam Sheth <jainamcsheth@gmail.com>
2020-07-02 12:38:21 +0530

    Updates

---
+++ precache-manifest.7acf9dc70cdca6265eef43d15e1d9769.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85cd619a8ed3acdb8de3312b3d895461",
+    "url": "/modello-ecommerce/index.html"
+  },
+  {
+    "revision": "582b618272d07208afe4",
+    "url": "/modello-ecommerce/static/css/main.77ec7804.chunk.css"
+  },
+  {
+    "revision": "fcb5b7c3ef7e2a9e1295",
+    "url": "/modello-ecommerce/static/js/2.59effa70.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/modello-ecommerce/static/js/2.59effa70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "582b618272d07208afe4",
+    "url": "/modello-ecommerce/static/js/main.1d2dc137.chunk.js"
+  },
+  {
+    "revision": "8c2f835f1388c649de4b",
+    "url": "/modello-ecommerce/static/js/runtime-main.e015dc27.js"
+  },
+  {
+    "revision": "0fee9966ef3f1e890c680ef4f9e9fb71",
+    "url": "/modello-ecommerce/static/media/bags-female-black-1.0fee9966.png"
+  },
+  {
+    "revision": "e9eed37bb2afc7bd2df6ece7e4eae120",
+    "url": "/modello-ecommerce/static/media/bags-female-pink-1.e9eed37b.png"
+  },
+  {
+    "revision": "438bd9752e7607fe84de825d31a5a03d",
+    "url": "/modello-ecommerce/static/media/dress-blue-1.438bd975.png"
+  },
+  {
+    "revision": "e886c2384913fe65688b3473fb833589",
+    "url": "/modello-ecommerce/static/media/dress-pink-1.e886c238.png"
+  },
+  {
+    "revision": "b9b561bb2f339f2ae19600014e0383c0",
+    "url": "/modello-ecommerce/static/media/jackets-black-1.b9b561bb.png"
+  },
+  {
+    "revision": "7f2791f9818b3c0a953a320c801a27e3",
+    "url": "/modello-ecommerce/static/media/jackets-female-red-1.7f2791f9.png"
+  },
+  {
+    "revision": "f4cb5d7e3fe331f100df9d008fd082f7",
+    "url": "/modello-ecommerce/static/media/jackets-green-1.f4cb5d7e.png"
+  },
+  {
+    "revision": "91ccaec1e253270365b983bb6ba71f2c",
+    "url": "/modello-ecommerce/static/media/jackets-red-1.91ccaec1.png"
+  },
+  {
+    "revision": "6315dfb00519c075413da0b61cba391c",
+    "url": "/modello-ecommerce/static/media/pants-formals-blue-1.6315dfb0.png"
+  },
+  {
+    "revision": "2782b7aadda7ec709bd74b535046ae7e",
+    "url": "/modello-ecommerce/static/media/shirt-blue-1.2782b7aa.png"
+  },
+  {
+    "revision": "ba179a02101e8b3278993d2c314f0a4f",
+    "url": "/modello-ecommerce/static/media/shirt-blue-2.ba179a02.jpg"
+  },
+  {
+    "revision": "0f6ecaaf28595041ad7af17f72a94b7a",
+    "url": "/modello-ecommerce/static/media/shirt-hawaiian-1.0f6ecaaf.png"
+  },
+  {
+    "revision": "bc510b4d9d423834e6624db53636a8ca",
+    "url": "/modello-ecommerce/static/media/shopping-model-background-1.bc510b4d.png"
+  },
+  {
+    "revision": "4f4162c10e239388e9a9168477c4a06f",
+    "url": "/modello-ecommerce/static/media/shopping-model-background-2.4f4162c1.jpg"
+  },
+  {
+    "revision": "4d45ec305e9483e9dc857f10b575b261",
+    "url": "/modello-ecommerce/static/media/tracks-red-1.4d45ec30.png"
+  },
+  {
+    "revision": "d977bc900038e95b7d7147c172350f6a",
+    "url": "/modello-ecommerce/static/media/trousers-blue-1.d977bc90.png"
+  }
+]);
